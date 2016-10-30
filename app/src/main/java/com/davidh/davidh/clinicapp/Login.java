4958b97f9c92f84bc6360a6bfe957c001f9e4d39
Stiven <sduque866@gmail.com>
2016-10-30 15:58:56 -0500

    se de la función de de lectura del usuario y verificación que los campos no sean vacios

--- app/src/main/java/com/davidh/davidh/clinicapp/Login.java
+++ app/src/main/java/com/davidh/davidh/clinicapp/Login.java
@@ -2,13 +2,38 @@ package com.davidh.davidh.clinicapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
-
+    EditText idUser, pass;
+    Button deposit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //"cambios"
+
+        idUser = (EditText)findViewById(R.id.et_login_idUser);
+        pass = (EditText)findViewById(R.id.et_login_pass);
+        deposit = (Button)findViewById(R.id.btn_login_deposit);
+
+        idUser.setHintTextColor(getResources().getColor(R.color.textApp));
+        pass.setHintTextColor(getResources().getColor(R.color.textApp));
+
+        deposit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (idUser.getText().toString().length() <= 0){
+                    idUser.setError(getString(R.string.login_error_idUser));
+                }else if (pass.getText().toString().length() <= 0) {
+                    pass.setError(getString(R.string.login_error_pass));
+                }
+                else {
+                 //hacer cualquier cosa
+                }
+            }
+        });
+
     }
 }
